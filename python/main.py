762402400894093324398835d85bdf1ac2324758
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2023-04-21 13:36:13 +0200

    Code cleanup

--- python/main.py
+++ python/main.py
@@ -3,10 +3,32 @@ import os
 import json
 import scipy.io as sio
 
-from scipy.linalg import cholesky
 from os.path import dirname, join as pjoin
-    
+from sksparse.cholmod import cholesky
 
+   
+def sparse_matrix_solver(A, b):
+    try: 
+        factor = cholesky(A)
+        x = factor(b)
+    except Exception as e:
+        print('Cholesky decomposition not supported: ' + str(e))
+        x = np.zeros(A.shape[0]) 
+    
+    return x
+
+
+def load_sparse_matrix(config):
+    print('matrices folder: ' + config['matrices_path'])
+    print(os.listdir(os.path.join(config['matrices_path'])))
+    
+    # Load the first sample .mat matrix 
+    mat = sio.loadmat(os.path.join(config['matrices_path'], 'apache2.mat'), struct_as_record=False)
+    sparse_mat = mat['Problem'][0][0].A
+    
+    return sparse_mat 
+    
+    
 
 
 def main():
@@ -16,19 +38,18 @@ def main():
     config_file = os.path.join(parent_dir, 'config.json')
     with open(config_file, 'r') as f:
         config = json.load(f)
-    print('matrices folder: ' + config['matrices_path'])
+        
    
-    # List all the available matrices
-    print(os.listdir(os.path.join(config['matrices_path'])))
+    A = load_sparse_matrix(config) 
     
-    # Load the first sample .mat matrix 
-    cfd2_mat = sio.loadmat(os.path.join(config['matrices_path'], 'cfd2.mat'), struct_as_record=False)
-    sparse_matrix = cfd2_mat['Problem'][0][0].A
+    b = np.ones(A.shape[0])
     
-    print(sparse_matrix)
+    x = sparse_matrix_solver(A, b)
     
-    # cfd2_dec_low = cholesky(cfd2_mat, lower=True)
+    print(x)
     
+    
+    a = 1
     
     
     
